fa9cfc0cb665b1f71e6e84e7843defad47e01e50
Ryan Hanks <ryanhanks98@gmail.com>
2018-10-20 22:51:41 +0000

    Updated scoring

--- gamemanager.py
+++ gamemanager.py
@@ -6,13 +6,28 @@ class GameManager(object):
         self.quit = False
 
     def play(self):
+        score = 0
         for i in range(3):
-            num1 = random.randint(1, 100)
-            num2 = random.randint(1, 100)
+            num1 = random.randint(10, 100)
+            num2 = random.randint(10, 100)
             user_input = input("{} x {} = ".format(num1,num2))
             ans = num1 * num2
-            score = int(user_input)/ans * 100
-            print(score)
+            tenp = ans * .1
+            fivep = ans * .05
+            estimate = int(user_input)
+            if estimate == ans:
+                score += 10
+                print("+10")
+            elif estimate > ans - fivep and estimate < ans + fivep:
+                score += 5
+                print("+5")
+            elif estimate > ans- tenp and estimate < ans + tenp:
+                score += 3
+                print("+3")
+
+            print(estimate/ans * 100)
+
+        print("Score is {}".format(score))
 
 
 
